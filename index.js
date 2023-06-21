38c14490cf972e5b4a7cd9c2b9c8a9ed60b261cc
vivian waithera <vivyannekori@gmail.com>
2023-06-21 09:26:31 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solutions in this file
+const names = [ "Guadalupe", "Ollie", "Aki"];
+
+function writeCards(names, event){
+  let messages = [];
+  for (let i = 0; i < names.length; i++){
+    messages.push("Thank you, " + names [i] + ", for the wonderful " + event + " gift!");
+
+  }
+  return messages;
+}
+console.log (writeCards(names, "surprise"));
+
+let i;
+function countDown(i){
+  while (i>-1){
+    console.log(i);
+    i--;
+  }
+}
+countDown(10);
